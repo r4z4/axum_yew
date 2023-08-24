b63fba31fdab131bd717e7d81e7d0e213cbf7d45
r4z4 <r4z4@gmail.com>
2023-08-24 00:10:53 +0000

    wasm bindgen work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <h1>{"Yew Heading"}</h1>
+            <div>{"Yew Div"}</div>
+            <div>
+                <ul>
+                    <li>{"First"}</li>
+                    <li>{"Second"}</li>
+                    <li>{"Third"}</li>
+                </ul>
+            </div>
+        </>
+    }
 }
